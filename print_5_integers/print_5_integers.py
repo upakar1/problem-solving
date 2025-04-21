5e042c3ed64d86cbeef6db310ec583e6d51f80ed
upakar1 <upakar21@gmail.com>
2025-04-21 06:56:25 +0545

    5 integers of a list are printed

---
+++ print_5_integers/print_5_integers.py
@@ -0,0 +1,8 @@
+fruits = ["apple" , "orange" , "mango" , "watermelon", "pineapple"]
+numbers = [10,20,30,40,50]
+
+for fruit in fruits:
+    print(fruit)
+
+for number in numbers:
+    print(number)
